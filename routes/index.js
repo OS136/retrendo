f63678e5a155374ba23246f952ebe6ea9bcef8e9
os136 <olenashereiko@gmail.com>
2025-01-10 14:03:54 +0100

    fix crash on repo

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 var path = require("path");
 const sqlite3 = require("sqlite3").verbose();
 
-const dbfilePath = path.resolve(process.cwd(), "./db/student-manager.db");
+// const dbfilePath = path.resolve(process.cwd(), "./db/products.db");
 
 // Ger oss tillgång till databasen
-const db = new sqlite3.Database(dbfilePath, sqlite3.OPEN_READWRITE, (err) => {
-  if (err) return console.error(err.message);
-});
+// const db = new sqlite3.Database(dbfilePath, sqlite3.OPEN_READWRITE, (err) => {
+//   if (err) return console.error(err.message);
+// });
 
 // GET /
 router.get("/", function (request, response) {});
