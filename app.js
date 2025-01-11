06c8342c676415dcf214b92e2e02646f123218bb
os136 <olenashereiko@gmail.com>
2025-01-11 02:51:10 +0100

    fixed html, added styling

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
+var adminRouter = require("./routes/admin");
 
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", [
+  path.join(__dirname, "views"),
+  path.join(__dirname, "views", "admin"),
+]);
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
@@ -19,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/admin", adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
