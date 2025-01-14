82e414b498211f821cc49f76fe2b3c4039f55b67
Veronica3999 <veronica_andersson-81@hotmail.com>
2025-01-14 19:43:05 +0100

    static product finish

--- routes/product.js
+++ routes/product.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var router = express.Router();
+const Database = require('better-sqlite3');
+const db = new Database('./db/Retrendo.db', {verbose: console.log});
+
 // var path = require("path");
 // const sqlite3 = require("sqlite3").verbose();
 
@@ -13,6 +16,38 @@ var router = express.Router();
 // GET /
 router.get("/", function (request, response) {
   response.render('product', {title: 'product'});
+});
+
+router.get('/product/:urlslug', function(req, res, next) {
+      const slug = req.params.urlslug;
+      console.log('slug:',slug);
+
+  const product = db.prepare(`
+    SELECT
+    id,
+    name,
+    product_type, 
+    color, 
+    price, 
+    size, 
+    brand, 
+    condition, 
+    image, 
+    description, 
+    slug, 
+    category, 
+    date
+    
+  FROM products
+  WHERE slug = ?
+  `).get(slug);
+  console.log(product);
+  if(!product) {
+    res.status(404).json({error: "Product not found"});
+  }
+  else{
+     res.json(product);
+  }   
 });
 
 module.exports = router;
