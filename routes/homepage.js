699c335fdb74189d78eabe598aa3b2812fb34a00
Cordelia Majaraite <rugilemajaraite@gmail.com>
2025-01-15 02:53:46 +0100

    Added homepage route, renamed startsida to homepage for both ejs and css, and updated path in app,js

---
+++ routes/homepage.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var router = express.Router();
+var path = require("path");
+const sqlite3 = require("sqlite3").verbose();
+
+// const dbfilePath = path.resolve(process.cwd(), "./db/products.db");
+
+// Ger oss tillgång till databasen
+// const db = new sqlite3.Database(dbfilePath, sqlite3.OPEN_READWRITE, (err) => {
+//   if (err) return console.error(err.message);
+// });
+
+// GET /
+router.get("/", (req, res) => {
+    res.render("homepage"); 
+  });
+  
+  module.exports = router;
