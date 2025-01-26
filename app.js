f1a2aac3d11e61f19f48e0e6fc09866d2a5fd2d3
Cordelia Majaraite <rugilemajaraite@gmail.com>
2025-01-26 22:43:43 +0100

    Implement search function

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var homepageRoutes = require("./routes/homepage");
 var adminRouter = require("./routes/admin");
+const searchRouter = require('./routes/search');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, "public")));
 //app.use("/", indexRouter);
 app.use("/", homepageRoutes);
 app.use("/admin", adminRouter);
+app.use('/search', searchRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
