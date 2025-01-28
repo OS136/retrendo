795c5314781e337868122c0cbfdaac7c077892b4
Isabelle Banjac <isabelle.banjac@hotmail.com>
2025-01-28 17:53:31 +0100

    Routes

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,24 @@
-const express = require("express");
-const router = express.Router();
+var express = require("express");
+var router = express.Router();
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database("./Retrendo.db");
 
-// Route to render categorieswoman.ejs
-router.get("/", (req, res) => {
-   res.render("categorieswoman"); // Use the exact EJS filename
+// Route för att visa Dam-kategorin
+router.get("/dam", function (req, res, next) {
+   console.log("Route /dam anropas");
+  const query = "SELECT * FROM products WHERE category = 'Dam'"; // Kategorin är 'Dam'
+  db.all(query, [], (err, products) => {
+    if (err) {
+      console.error("Fel vid hämtning av produkter:", err.message);
+      return res.status(500).send("Fel vid hämtning av data");
+    } else {
+      res.render("categorieswoman", { products }); // Skickar produkterna till vyn
+    }
+  });
 });
 
+
+
 module.exports = router;
+
+
