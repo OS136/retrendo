a5d5654dcd17cb446d5cade82c5e7928b3998cec
Isabelle Banjac <isabelle.banjac@hotmail.com>
2025-01-29 19:13:03 +0100

    Fixed route, addes css

--- routes/categories.js
+++ routes/categories.js
@@ -1,33 +1,35 @@
-// Modified categories.js route
-// categories.js
-const express = require("express");
-const router = express.Router();
-const path = require('path');
-const sqlite3 = require('sqlite3').verbose();
+var express = require("express");
+var router = express.Router();
+const path = require("path");
+var sqlite3 = require("sqlite3").verbose();
+const zlib = require("zlib");
 
-// Create database connection directly in categories.js for now
-const db = new sqlite3.Database(path.join(__dirname, '..', 'Retrendo.db'), sqlite3.OPEN_READWRITE, (err) => {
-  if (err) {
-    console.error("Database connection error:", err.message);
-  } else {
-    console.log("Connected to the database successfully");
-  }
-});
 
-router.get("/dam", function (req, res, next) {
-  console.log("Route /dam anropas");
-  
-  const query = "SELECT * FROM products WHERE category = 'Dam'";
-  console.log("Executing query:", query);
-  
-  db.all(query, [], (err, products) => {
+const db = new sqlite3.Database(
+  path.join(__dirname, "../db/Retrendo.db"),
+  (err) => {
     if (err) {
-      console.error("Error fetching products:", err.message);
-      return res.status(500).send("Error fetching data");
+      console.error("Error connecting to database:", err);
     }
-    
-    console.log("Found products:", products);
-    res.render("categorieswoman", { products: products || [] });
+  }
+);
+
+router.get("/:category", function (req, res) {
+  const category =
+    req.params.category.charAt(0).toUpperCase() +
+    req.params.category.slice(1).toLowerCase();
+
+  const query = "SELECT * FROM products WHERE category = ?";
+  db.all(query, [category], (err, products) => {
+    if (err) {
+      console.error("Fel vid hämtning av produkter:", err.message);
+      return res.status(500).send("Fel vid hämtning av data");
+    }
+
+    res.render("categorieswoman", {
+      products: products,
+      categoryName: category,
+    });
   });
 });
 
