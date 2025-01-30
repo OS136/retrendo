46d951ce499c774f9f8d18dd93496e6173346f5a
Isabelle Banjac <isabelle.banjac@hotmail.com>
2025-01-30 14:23:36 +0100

    Removed one extra path

--- routes/categories.js
+++ routes/categories.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var router = express.Router();
 const path = require("path");
-const path = require("path");
 var sqlite3 = require("sqlite3").verbose();
 const zlib = require("zlib");
 
