c1d6d443b0d96f114259eb3e5167cd0fef991a6a
Veronica Andersson <veronica_andersson-81@hotmail.com>
2025-01-31 08:03:45 +0100

    emptyspace fix

--- routes/confirmation.js
+++ routes/confirmation.js
@@ -3,8 +3,6 @@ var router = express.Router();
 var path = require("path");
 const sqlite3 = require("sqlite3").verbose();
 
-
-
 router.get('/', function(request, response){
 
 response.render("confirmation", {
